42b7b4f94078dfc95c68a835343f96fc1e931eb7
Srinith <srinith@Sriniths-Laptop.local>
2025-09-02 14:11:37 +0530

    made some changes to the code to make it more efficient

--- tutorial-03/Q2.cpp
+++ tutorial-03/Q2.cpp
@@ -1,67 +1,47 @@
-#include <iostream> 
+#include <iostream>
 #include <cmath>
 #include <vector>
 #include <algorithm>
-using namespace std ;
-vector<pair<int,int>>points ;
-vector<pair<int,int>>ans ;
+using namespace std;
 
-double perpendicularDistanceUp(pair<int,int> left, pair<int,int> right, pair<int,int> p) {
-    if(p.second > left.second || p.second > right.second){
-        double num = fabs((right.second - left.second) * p.first 
-                        - (right.first - left.first) * p.second 
-                        + right.first * left.second 
-                        - right.second * left.first);
-        double den = sqrt(pow(right.second - left.second, 2) 
-                        + pow(right.first - left.first, 2));
-        return num / den;
-    }
-    else{
-        return 0 ;
-    }
-}
-double perpendicularDistanceDown(pair<int,int> left, pair<int,int> right, pair<int,int> p) {
-    if(p.second < left.second || p.second < right.second){
-        double num = fabs((right.second - left.second) * p.first 
-                        - (right.first - left.first) * p.second 
-                        + right.first * left.second 
-                        - right.second * left.first);
-        double den = sqrt(pow(right.second - left.second, 2) 
-                        + pow(right.first - left.first, 2));
-        return num / den;
-    }
-    else{
-        return 0 ;
-    }
+vector<pair<int,int>> points;
+vector<pair<int,int>> ans;
+
+double perpendicularDistance(pair<int,int>& left, pair<int,int>& right, pair<int,int>& p, char dir) {
+    bool valid = (dir == 'U') ? (p.second > left.second || p.second > right.second)
+                              : (p.second < left.second || p.second < right.second);
+    if (!valid) return 0;
+
+    double num = fabs((right.second - left.second) * p.first 
+                    - (right.first - left.first) * p.second 
+                    + right.first * left.second 
+                    - right.second * left.first);
+    double den = hypot(right.second - left.second, right.first - left.first);
+    return num / den;
 }
 
-void recursive (int l , int r , char dir ){
-    if (l == r ){   
-        return ;
-    }
-    double maxd = 0 ;
-    double temp = 0 ;
-    int index = 0 ;
-    for(int i = l+1 ; i<r ; i++){
-        if(dir == 'U'){
-            temp = perpendicularDistanceUp(points[l], points[r], points[i]);
-        }
-        else{
-            temp = perpendicularDistanceDown(points[l], points[r], points[i]);
-        }
-        if(temp > maxd){
-            maxd = temp ; 
-            index = i ;
+void recursive(int l, int r, char dir) {
+    if (l == r) return;
+
+    double maxd = 0;
+    int index = -1;
+
+    for (int i = l + 1; i < r; i++) {
+        double temp = perpendicularDistance(points[l], points[r], points[i], dir);
+        if (temp > maxd) {
+            maxd = temp;
+            index = i;
         }
     }
-    if(maxd != 0){
+
+    if (index != -1) {
         ans.push_back(points[index]);
-        recursive(l , index , dir);
-        recursive(index , r , dir);
+        recursive(l, index, dir);
+        recursive(index, r, dir);
     }
 }
 
-int main(void){
+int main() {
     int n ;
     cout << "what are the number of points : " ;
     cin >> n ;
@@ -78,13 +58,20 @@ int main(void){
         cin >> y ;
         points.push_back({x , y});
     }
-    sort(points.begin(), points.end());
-    ans.push_back(points[0]);
-    ans.push_back(points[points.size()-1]);
-    recursive(0 , points.size()-1 , 'U') ;
-    recursive(0 , points.size()-1 , 'D') ;
 
-    // sorting with the centroid so that the convex hull is printed in the counter clock wise order 
+    sort(points.begin(), points.end());
+    ans.reserve(n);
+    ans.push_back(points.front());
+    ans.push_back(points.back());
+
+    recursive(0, points.size() - 1, 'U');
+    recursive(0, points.size() - 1, 'D');
+
+    // Remove duplicates
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+    // Sort counter-clockwise using centroid
     double cx = 0, cy = 0;
     for (auto &p : ans) {
         cx += p.first;
@@ -92,14 +79,14 @@ int main(void){
     }
     cx /= ans.size();
     cy /= ans.size();
-    sort(ans.begin(), ans.end(), [cx, cy](pair<int,int> a, pair<int,int> b) {
-        double angA = atan2(a.second - cy, a.first - cx);
-        double angB = atan2(b.second - cy, b.first - cx);
-        return angA < angB;
+
+    sort(ans.begin(), ans.end(), [cx, cy](auto &a, auto &b) {
+        return atan2(a.second - cy, a.first - cx) < atan2(b.second - cy, b.first - cx);
     });
-    cout << "the convex hull in counter clockwise direction" << endl ;
-    for(auto &p : ans){
-        cout << p.first << "," << p.second << endl ;
+
+    for (auto &p : ans) {
+        cout << p.first << " " << p.second << "\n";
     }
-    return 0 ;
+
+    return 0;
 }
